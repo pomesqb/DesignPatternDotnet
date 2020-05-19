44c48d4734ebc5c0f6905fb16eab7ac26f6b9294
buckblader <buckblader@outlook.com>
2020-05-19 15:33:00 +0800

    InitCommit

---
+++ Memento/Editor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Memento
+{
+    class Editor
+    {
+        private Stack<string> _stack = new Stack<string>();
+
+        private string _content;
+
+        public string Content
+        {
+            get
+            {
+                return _stack.Peek();
+            }
+            set
+            {
+                _stack.Push(value);
+            }
+        }
+
+        public void Undo()
+        {
+            _stack.Pop();
+        }
+    }
+}
---
+++ Memento/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Memento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Editor editor = new Editor();
+            editor.Content = "a";
+            editor.Content = "b";
+            editor.Content = "c";
+            editor.Content = "d";
+            editor.Undo();
+            editor.Undo();
+
+            Console.WriteLine("Editor Content:" + editor.Content);
+        }
+    }
+}
