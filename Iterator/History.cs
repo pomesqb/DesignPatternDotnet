510ad3b60bcbdb687a43952e7d16b847c8118c98
pomesqb <pomesqb@gmail.com>
2020-05-21 15:16:11 +0800

    Iterator Problem

---
+++ Iterator/History.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Iterator
+{
+    class History
+    {
+        private List<string> _historyData = new List<string>();
+
+        public List<string> GetHistoryData
+        {
+            get => _historyData;
+            set => _historyData = value;
+        }
+
+        public void Push(string item)
+        {
+            _historyData.Add(item);
+        }
+
+        public string Pop()
+        {
+            var lastItemIndex = _historyData.Count - 1;
+            var result = _historyData[lastItemIndex];
+            _historyData.RemoveAt(lastItemIndex);
+
+            return result;
+        }
+    }
+}
---
+++ Iterator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Iterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var history = new History();
+            history.Push("against");
+            history.Push("all");
+            history.Push("gods");
+
+            for (int i = 0; i < history.GetHistoryData.Count; i++)
+            {
+                Console.WriteLine(history.GetHistoryData[i]);
+            }
+        }
+    }
+}
